5be6e609082ce44017eef04febfb3a2782714ba1
Simone Ceredi <ceredi.simone.iti@gmail.com>
2023-02-27 11:17:47 +0100

    Created simpleBankAccountWithAtm

---
+++ pps-lab01-intellij-basic-example/test/lab01/example/model/SimpleBankAccountWithAtmTest.java
@@ -0,0 +1,12 @@
+package lab01.example.model;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleBankAccountWithAtmTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
