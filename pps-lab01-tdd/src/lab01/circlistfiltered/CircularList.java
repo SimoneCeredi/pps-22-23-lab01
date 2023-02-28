f2b436eef3cd90ebd7d5c4cd60ca360649bedae1
Simone Ceredi <ceredi.simone.iti@gmail.com>
2023-02-28 19:50:16 +0100

    Refactor, added javadoc

--- pps-lab01-tdd/src/lab01/circlistfiltered/CircularList.java
+++ pps-lab01-tdd/src/lab01/circlistfiltered/CircularList.java
@@ -1,9 +1,24 @@
 package lab01.circlistfiltered;
 
 public interface CircularList {
+    /**
+     * Checks if the list is empty
+     *
+     * @return true if the list is empty, false otherwise
+     */
     boolean isEmpty();
 
+    /**
+     * Provides the current size of the list
+     *
+     * @return the size of the list
+     */
     int size();
 
+    /**
+     * Adds an element to the list, namely, after the last inserted one.
+     *
+     * @param element the element to be added to the list
+     */
     void add(int element);
 }
